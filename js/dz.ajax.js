a99fa62c6ad5fbb9412650560876b5d0b2f478f1
Jeroen Degloire <jeroendegloire@MacBook-Pro-van-Jeroen.local>
2023-06-06 19:00:37 +0200

    Changes

--- js/dz.ajax.js
+++ js/dz.ajax.js
@@ -21,6 +21,26 @@ function contactForm()
     window.expiredRecaptchaCallback = function () {
         $('input[data-recaptcha]').val("").trigger('change');
     }
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		const myForm = event.target;
+		const formData = new FormData(myForm);
+
+		fetch("/", {
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: new URLSearchParams(formData).toString(),
+		})
+			.then(() => console.log("Form successfully submitted"))
+			.catch((error) => alert(error));
+	};
+
+	document
+		.querySelector("form")
+		.addEventListener("submit", handleSubmit);
+
 	'use strict';
 	var msgDiv;
 	$(".dzForm").submit(function(e)
